4d19705a135365d175fffa2d406e13e606fcdbcc
Santiago Ravasi <ravasiravasisantiago@gmail.com>
2025-12-21 17:27:05 -0300

    sweetAlert2

---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+let arraypersonas = [];
+
+const usuariosGuardados = localStorage.getItem("ObjetoUsuarios");
+if (usuariosGuardados) {
+    arraypersonas = JSON.parse(usuariosGuardados);
+    console.log("Usuarios cargados desde Storage:", arraypersonas);
+}
+else {
+    console.log("No hay usuarios guardados en localStorage.");
+};
+
+// //Usuario de prueba admin
+// arraypersonas.push({ 
+//     nombreUsuario: "Admin", 
+//     clave: "1234",
+// });
+// console.log(arraypersonas);
+
+const vistaLogin = document.getElementById("vistaLogin");
+const vistaOnbording = document.getElementById("vistaOnbording");
+const vistaAcceso = document.getElementById("vistaAcceso");
+
+const btnRegistro = document.getElementById("botonRegistro");
+const btnOnbording = document.getElementById("botonOnbording");
+const btnAcceso = document.getElementById("botonAcceso");
+
+
+const formularioLogin = document.getElementById("formularioLogin");
+const formularioOnbording = document.getElementById("formularioOnbording");
+
+const mensajeOnboarding = document.getElementById("mensajeOnboarding");
+const saludoUsuario = document.getElementById("Saludo")
+
+
+
+
+function cambiarPantalla(nombreVista) {
+    vistaLogin.classList.add('oculto');
+    vistaOnbording.classList.add('oculto');
+    vistaAcceso.classList.add('oculto');
+
+    if (nombreVista === 'login') {
+        vistaLogin.classList.remove('oculto');
+    } else if (nombreVista === 'registro') {
+        vistaOnbording.classList.remove('oculto');
+    } else if (nombreVista === 'acceso') {
+        vistaAcceso.classList.remove('oculto');
+    }
+};
+
+btnRegistro.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    cambiarPantalla('registro');
+});
+
+btnOnbording.addEventListener('click', (evento) => {
+    evento.preventDefault();
+    cambiarPantalla('login');
+});
+
+//Onboarding
+formularioOnbording.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const nuevoUsuario = document.getElementById('usuarioOnbording').value;
+    const nuevaClave = document.getElementById('passOnboarding').value;
+
+    const usuarioExiste = arraypersonas.find(objeto => objeto.nombreUsuario === nuevoUsuario);
+
+    if (usuarioExiste) {
+        mensajeOnboarding.textContent = "El nombre de usuario ya existe, elige otro nombre.";
+        mensajeOnboarding.className = "error";
+    } else {
+
+        arraypersonas.push({ nombreUsuario: nuevoUsuario, clave: nuevaClave });
+
+        mensajeOnboarding.textContent = "¡Usuario registrado! Ahora inicia sesión.";
+        mensajeOnboarding.className = "usuarioCreado";
+        localStorage.setItem("ObjetoUsuarios", JSON.stringify(arraypersonas));
+
+        console.log("Lista actual de usuarios:", arraypersonas);
+    }
+});
+
+// LOGIN 
+formularioLogin.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const usuarioIngresado = document.getElementById('usuarioLogin').value;
+    const claveIngresada = document.getElementById('passLogin').value;
+
+    const usuarioEncontrado = arraypersonas.find(u => u.nombreUsuario === usuarioIngresado && u.clave === claveIngresada);
+
+    if (usuarioEncontrado) {
+        Swal.fire({
+            title: "Hola " + usuarioEncontrado.nombreUsuario + "! , deseas ingreasr al reprodcutor?",
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: "Si quiero",
+            denyButtonText: `No quiero`,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                    cambiarPantalla('acceso');
+                setTimeout(function () {
+                    window.location.href = "/pages/Reproductor.html";
+                }, 3000);
+            }
+        });
+    } else {
+        Swal.fire({
+            title: '¡Credenciales incorrectas!',
+            text: 'Ingresa nuevamente el usuario y la contraseña',
+            icon: 'error',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Reintentar'
+        });
+    };
+});
---
+++ assets/js/scriptReprodcutor.js
@@ -0,0 +1,98 @@
+const canciones = [
+    { 
+        nombre: "Profe Lucas Theme", 
+        img: "/assets/img/img1.webp", 
+        archivo: "/assets/audio/profe_lucas.webm" 
+    },
+    {
+        nombre: "Crimen", 
+        img: "/assets/img/img2.webp", 
+        archivo: "/assets/audio/crimen.webm" 
+    },
+    { 
+        nombre: "Promesas sobre le bidet", 
+        img: "/assets/img/img3.webp", 
+        archivo: "/assets/audio/promesas_sobre_el_bidet.webm" 
+    },
+    { 
+        nombre: "Tu amor", 
+        img: "/assets/img/img4.webp", 
+        archivo: "/assets/audio/tu_amor.webm" 
+    },
+    { 
+        nombre: "Tus ojos",
+        img: "/assets/img/img5.webp", 
+        archivo: "/assets/audio/tus_ojos.webm" 
+    },
+    { 
+        nombre: "Barro Talvez",
+        img: "/assets/img/img6.webp", 
+        archivo: "/assets/audio/barro_talvez.webm" 
+    }
+];
+
+let indice = 0;
+let audio = new Audio();
+
+
+const nombreCancion = document.getElementById("nombreCancion");
+const imgCancion = document.getElementById("imgCancion");
+const progreso = document.getElementById("progreso");
+const btnPlay = document.getElementById("play");
+const btnAnterior = document.getElementById("anterior");
+const btnSiguiente = document.getElementById("siguiente");
+const btnLogout = document.getElementById("botonLogout");
+
+
+
+cargarCancion(indice);
+
+function cargarCancion(i) {
+    audio.src = canciones[i].archivo;
+    nombreCancion.innerText = canciones[i].nombre;
+    imgCancion.src = canciones[i].img;
+}
+
+btnPlay.onclick = function () {
+    if (audio.paused) {
+        audio.play();
+        btnPlay.innerText = "⏸";
+    } else {
+        audio.pause();
+        btnPlay.innerText = "▶";
+    }
+};
+
+btnSiguiente.onclick = function () {
+    indice++;
+    if (indice >= canciones.length) indice = 0;
+    cargarCancion(indice);
+    audio.play();
+    btnPlay.innerText = "⏸";
+};
+
+btnAnterior.onclick = function () {
+    indice--;
+    if (indice < 0) indice = canciones.length - 1;
+    cargarCancion(indice);
+    audio.play();
+    btnPlay.innerText = "⏸";
+};
+
+// Actualizar barra de progreso (para esto necesite ayuda de la IA 😶 )
+setInterval(() => {
+    if (audio.duration) {
+        const porcentaje = (audio.currentTime / audio.duration) * 100;
+        progreso.style.width = porcentaje + "%";
+    }
+}, 300);
+
+// Pasar a la siguiente canción cuando termina
+audio.onended = function () {
+    btnSiguiente.onclick();
+};
+
+// Botón de cerrar sesión
+btnLogout.addEventListener('click', () => {
+    window.location.href = "../index.html";
+});
